3420fb7329e19a797b78121dc1040ec8543d9f0e
vivial650 <38332182+vivial650@users.noreply.github.com>
2018-04-14 01:10:12 +0300

    Update TitlesFrame.java
    Изменение параметра TitlesPanel(78) на TitlesPanel(11)

--- TitlesFrame.java
+++ TitlesFrame.java
@@ -14,7 +14,7 @@ extends JFrame {
     private void initUI() {
         this.setTitle("\u041a\u0440\u0438\u0432\u044b\u0435 \u0444\u0438\u0433\u0443\u0440\u044b");
         this.setDefaultCloseOperation(3);
-        this.add(new TitlesPanel(78));
+        this.add(new TitlesPanel(11));
         this.setSize(350, 350);
         this.setLocationRelativeTo(null);
     }
