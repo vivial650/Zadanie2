f1865fbe2ac1aa3ae2bb9344c60fbc1f1b36ae0f
vivial650 <38332182+vivial650@users.noreply.github.com>
2018-05-15 13:50:08 +0300

    Update TitlesFrame.java

--- TitlesFrame.java
+++ TitlesFrame.java
@@ -14,10 +14,18 @@ extends JFrame {
         this.initUI();
     }
 
+	 /**
+     * 
+     * создано 2 перечисления :типы фигур и их атрибуты
+     */
+    public static enum ShapeTypes{ STAR_3ARMS, STAR_5ARMS, RECTANGLE, TRIANGLE, CIRCLE; }        
+    public static  enum ShapeAttribute{STROKE_3,  STROKE_7, GRADIENT, COLOR_RED, NO_ATTRIBUTE; }
+	
     private void initUI() {
         this.setTitle("\u041a\u0440\u0438\u0432\u044b\u0435 \u0444\u0438\u0433\u0443\u0440\u044b");
         this.setDefaultCloseOperation(3);
-        this.add(new TitlesPanel(11));
+        //this.add(new TitlesPanel(11));
+	this.add(new TitlesPanel(ShapeTypes.STAR_3ARMS, ShapeAttribute.STROKE_3));   //добавлено    
         this.setSize(350, 350);
         this.setLocationRelativeTo(null);
     }
